68822e316df57e716e919b5e5baac27e65be435e
Brendan Ochs <brendanochs@MacBook-Pro.cable.rcn.com>
2021-02-17 19:49:33 -0600

    added create page

--- src/app/people.service.ts
+++ src/app/people.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { tap, distinct, map, filter } from 'rxjs/operators';
+import { tap, map } from 'rxjs/operators';
 import { Person } from './types';
 
 @Injectable({
@@ -9,17 +9,47 @@ import { Person } from './types';
 })
 export class PeopleService {
 
-  allPeople: BehaviorSubject<Person[]> = new BehaviorSubject([]);
-  allGenders: BehaviorSubject<string[]> = new BehaviorSubject([]);
+  $allPeople: BehaviorSubject<Person[]> = new BehaviorSubject([]);
+  $allGenders: BehaviorSubject<string[]> = new BehaviorSubject([]);
+
+  allPeople: Person[] = [];
 
   constructor(private http: HttpClient) { }
 
   getData() {
     this.http.get<Person[]>('assets/people.json').pipe(
-      tap(people => this.allPeople.next(people)),
+      tap(people => {
+        this.$allPeople.next(people);
+        this.allPeople = people;
+      }),
       map(people => people.map(person => person.gender)),
-      map(genders => this.allGenders.next([...new Set(genders)]))
+      map(genders => this.$allGenders.next([...new Set(genders)]))
     ).subscribe();
+  }
+  
+  create(p: Person) {
+    p.id = Math.max(...this.allPeople.map(person => person.id)) + 1;
+    p.createdDate = new Date();
+    this.allPeople.push(p);
+    this.$allPeople.next(this.allPeople);
+
+    //add person to table
+  }
+
+  update(id: number, edits: Partial<Person>) {
+    const index = this.allPeople.findIndex(p => p.id == id);
+    this.allPeople[index] = {...this.allPeople[index], ...edits} 
+    this.$allPeople.next(this.allPeople);
+    
+    //update a person in the table
+  }
+
+  delete(id: number) {
+    const index = this.allPeople.findIndex(p => p.id == id);
+    this.allPeople = [...this.allPeople.slice(0, index), ...this.allPeople.slice(index + 1)];
+    this.$allPeople.next(this.allPeople);
+    
+    //delete a person from the table
   }
 
   onlyUnique(value, index, self) {
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -12,7 +12,11 @@ export class Tab1Page {
   allPeople: Observable<Person[]>;
   allGenders: Observable<string[]>;
   constructor(private people: PeopleService) {
-    this.allPeople = this.people.allPeople;
-    this.allGenders = this.people.allGenders;
+    this.allPeople = this.people.$allPeople;
+    this.allGenders = this.people.$allGenders;
+  }
+
+  delete(id: number) {
+    this.people.delete(id);
   }
 }
