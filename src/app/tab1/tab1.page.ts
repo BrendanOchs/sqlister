e2baa8dc64631e7eba9a54cccdcf197b7d01c465
Brendan Ochs <brendanochs@MacBook-Pro.cable.rcn.com>
2021-02-19 00:23:05 -0600

    add screen for no data

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { PeopleService } from '../people.service';
+import { Person } from '../types';
 
 @Component({
   selector: 'app-tab1',
@@ -6,5 +10,8 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  constructor() { }
+  peopleData: Observable<Person[]>;
+  constructor(private ps: PeopleService) { 
+    this.peopleData = this.ps.$allPeople;
+  }
 }
